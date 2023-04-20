82ab11f9c9391fc61a614205d5724ca9ccb200e7
Your Name <son.nt@3si.vn>
2023-04-20 15:06:21 +0700

    add homepage travel

---
+++ Travel/src/components/TopDestination/TopDestination.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="top-destination">
+    <span class="top-des-first">
+      <p>Top Destination</p>
+    </span>
+    <h3 class="top-des-second">
+      Discover your love
+    </h3>
+  </div>
+  <div class="discover-desination">
+    <div class="detail-descover">
+      <div class="detail-image" v-for="items in listItemImage" :key="items">
+        <img :src="items.src" :alt="items.alt" />
+        <div class="detail-text">
+          <p class="detail-top">
+            {{ items.title }}
+          </p>
+          <div class="detail-subtext">
+            <p class="sub1">{{ items.subname }}</p>
+            <p class="sub2">{{ items.price }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script scoped>
+import './style-top.css';
+var anh1 = ('/src/assets/anh1.png');
+var anh2 = ('/src/assets/anh2.png');
+var anh3 = ('/src/assets/anh3.png');
+var anh4 = ('/src/assets/anh4.png');
+var anh5 = ('/src/assets/anh5.png');
+var anh6 = ('/src/assets/anh6.png');
+export default {
+  data() {
+    return {
+      listItemImage: [
+        { src: anh1, alt: "anh1", title: "Taman Nasional Komodo", subname: "7 days for trips", price: "Rp. 7,5jt" },
+        { src: anh2, alt: "anh2", title: "Taman Nasional Bunaken", subname: "9 days for trips", price: "Rp. 10,5jt" },
+        { src: anh3, alt: "anh3", title: "Raja Ampat, Papua Barat", subname: "14 days for trips", price: "Rp. 20,5jt" },
+        { src: anh4, alt: "anh4", title: "Kepulauan Wakatobi", subname: "12 days for trips", price: "Rp. 9,5jt" },
+        { src: anh5, alt: "anh5", title: "Gili Trawangan, Lombok", subname: "7 days for trips", price: "Rp. 6,5jt" },
+        { src: anh6, alt: "anh6", title: "Taman Nasional Bromo", subname: "7 days for trips", price: "Rp. 9,5jt" },
+      ]
+    }
+  }
+}
+</script>
---
+++ Travel/src/components/TravelPoints/TravelPoints.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="travels point col-md-3 img">
+    <div class="travel content df">
+      <div class="travel-image df">
+        <img v-for="img in travelImage" :key="img" :src="img.src" :alt="img.alt" />
+      </div>
+      <div class="travel-text">
+        <span class="travel-media">Travelers Point</span>
+        <h2>We help to find your dream place</h2>
+        <p class="text info mg20">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet
+          luctus venenatis, lectus
+          magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est
+          ullamcorper eget nulla facilisi</p>
+        <div class="travel-number">
+          <div class="number1 info1">
+            <label class="number1">100+</label>
+            <span class="number1-text">Holiday Package</span>
+            <label class="number1">68</label>
+            <span class="number1-text">Elite Transportation</span>
+          </div>
+          <div class="number2 info2">
+            <label class="number2">172</label>
+            <span class="number2-text">Hotel</span>
+            <label class="number2">32M+</label>
+            <span class="number2-text">we help to find your dream place</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import "./style.css";
+var image1 = ('/src/assets/travel1.png');
+var image2 = ('/src/assets/travel2.png');
+var image3 = ('/src/assets/travel3.png');
+export default {
+  data() {
+    return {
+      travelImage: [
+        { src: image1, alt: "travel1" },
+        { src: image2, alt: "travel2" },
+        { src: image3, alt: "travel3" },
+      ]
+    }
+  }
+}
+</script>
